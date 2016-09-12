2b102c72b28e7deed3b6770634d2a9543fccd604
James Chambers <james@jameschambers.com>
2016-09-11 19:10:30 -0500

    rename config provider

--- src/AlpineSkiHouse.Web/AlpineConfigurationBuilder.cs
+++ src/AlpineSkiHouse.Web/AlpineConfigurationBuilder.cs
@@ -4,17 +4,17 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
-
+using AlpineSkiHouse.Configuration;
 
 namespace AlpineSkiHouse
 {
-    public class ConfigBuilder
+    public class AlpineConfigurationBuilder
     {
         public static IConfigurationRoot Build(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
                .SetBasePath(env.ContentRootPath)
+               .AddCsrInformationFile("config\\TPC_ONLINE_AGENTS.INFO", reloadOnChange: true)
                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
