338f8cf0df6aa83fd78c25c1af0ba823e92001bc
David Paquette <dave@davepaquette.com>
2016-09-14 17:50:18 -0600

    Tag Helper for Login Provider Button

---
+++ src/AlpineSkiHouse.Web/TagHelpers/LoginProviderButtonTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlpineSkiHouse.TagHelpers
+{
+    [HtmlTargetElement("button", Attributes = "ash-login-provider")]
+    public class LoginProviderButtonTagHelper : TagHelper
+    {
+        [HtmlAttributeName("ash-login-provider")]
+        public AuthenticationDescription LoginProvider { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("type", "submit");
+            output.Attributes.SetAttribute("name", "provider");
+            output.Attributes.SetAttribute("value", LoginProvider.AuthenticationScheme);
+            output.Attributes.SetAttribute("title", $"Log in using your {LoginProvider.DisplayName} account");            
+
+            //TODO: Extension method to merge class attributes because this code is annoying
+            string classValue = "btn btn-default";
+            if (output.Attributes.ContainsName("class"))
+            {
+                classValue = $"{output.Attributes["class"].Value} {classValue}";
+            }
+            output.Attributes.SetAttribute("class", classValue);
+
+            output.Content.SetContent(LoginProvider.AuthenticationScheme);
+        }
+    }
+}
