a6298ffc112d8fae2468036dccf615fa87f5a86c
David Paquette <dave@davepaquette.com>
2016-09-23 20:01:51 -0600

    Place holders for lots of awesome tests

---
+++ test/AlpineSkiHouse.Web.Test/Controllers/SkiControllerTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlpineSkiHouse.Web.Tests.Controllers
+{
+    public class SkiControllerTests
+    {
+    }
+}
---
+++ test/AlpineSkiHouse.Web.Test/Services/CsrInformationServiceTests.cs
@@ -0,0 +1,57 @@
+﻿using AlpineSkiHouse.Configuration;
+using AlpineSkiHouse.Services;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AlpineSkiHouse.Web.Tests.Services
+{
+    public class CsrInformationServiceTests
+    {
+        public class GivenThereIsAtLeastOneRepresentativeOnline
+        {            
+            public static readonly List<object[]> options = new List<object[]>
+               {
+                    new object[] {Options.Create(new CsrInformationOptions { OnlineRepresentatives = 1} )},
+                    new object[] {Options.Create(new CsrInformationOptions { OnlineRepresentatives = 2} )},
+                    new object[] {Options.Create(new CsrInformationOptions { OnlineRepresentatives = 3} )},
+                    new object[] {Options.Create(new CsrInformationOptions { OnlineRepresentatives = 1000} )},
+                    new object[] {Options.Create(new CsrInformationOptions { OnlineRepresentatives = 100000} )}
+               };
+            
+            [Theory]
+            [MemberData(nameof(options))]
+            public void CallCenterOnlineShouldBeTrue(IOptions<CsrInformationOptions> options)
+            {
+                var service = new CsrInformationService(options);
+                Assert.True(service.CallCenterOnline);
+            }
+
+            [Theory]
+            [MemberData(nameof(options))]
+            public void OnlineRepresentativesShouldMatchOptionsSource(IOptions<CsrInformationOptions> options)
+            {
+                var service = new CsrInformationService(options);
+                Assert.Equal(options.Value.OnlineRepresentatives, service.OnlineRepresentatives);
+            }
+        }
+
+        public class GivenThereAreNoRepresentativesOnline
+        {
+            private IOptions<CsrInformationOptions> options;
+
+            public GivenThereAreNoRepresentativesOnline()
+            {
+                options = Options.Create(new CsrInformationOptions());
+                options.Value.OnlineRepresentatives = 0;
+            }
+
+            [Fact]
+            public void CallCenterOnlineShouldBeFalse()
+            {
+                var service = new CsrInformationService(options);
+                Assert.False(service.CallCenterOnline);
+            }
+        }
+    }
+}
