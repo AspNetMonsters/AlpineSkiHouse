39d97e0c82c193a21a14163bdc36d6ddaad98dc6
Monster Dave <david.paquette@usask.ca>
2016-09-28 20:34:19 -0600

    Wrong parameters. That didn't even compile

--- test/AlpineSkiHouse.Web.Test/ViewComponents/CallCenterStatusViewComponentTests.cs
+++ test/AlpineSkiHouse.Web.Test/ViewComponents/CallCenterStatusViewComponentTests.cs
@@ -40,7 +40,7 @@ namespace AlpineSkiHouse.Web.Tests.ViewComponents
 
                 Assert.IsType<ViewViewComponentResult>(result);
                 var viewResult = result as ViewViewComponentResult;
-                Assert.Null("Default", viewResult.ViewName);
+                Assert.Null(viewResult.ViewName);
             }
         }
     }
