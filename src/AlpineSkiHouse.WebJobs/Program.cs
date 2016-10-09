092923078ec4028d1054b42eb9d404ea38f28236
James Chambers <james@jameschambers.com>
2016-10-09 13:56:23 -0500

    adding webjob

---
+++ src/AlpineSkiHouse.WebJobs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Newtonsoft.Json.Linq;
+using ImageProcessorCore;
+using Microsoft.Azure.WebJobs;
+
+namespace AlpineSkiHouse.WebJobs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            JobHost host = new JobHost();
+            host.RunAndBlock();
+        }
+    }
+}
