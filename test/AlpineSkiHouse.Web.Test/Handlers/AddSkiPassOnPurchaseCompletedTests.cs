2120f8c44285d1cdf11498cee2c51ab484afa44b
Simon Timms <stimms@gmail.com>
2016-10-11 23:23:25 -0600

    Additional tests

--- test/AlpineSkiHouse.Web.Test/Handlers/AddSkiPassOnPurchaseCompletedTests.cs
+++ test/AlpineSkiHouse.Web.Test/Handlers/AddSkiPassOnPurchaseCompletedTests.cs
@@ -17,24 +17,28 @@ namespace AlpineSkiHouse.Web.Tests.Handlers
     {
         public class When_handling_purchase_completed
         {
+            PassPurchased passPurchased = new PassPurchased
+            {
+                CardId = 1,
+                PassTypeId = 2,
+                DiscountCode = "2016springpromotion",
+                PricePaid = 200m
+            };
+
+            private static PassContext GetContext()
+            {
+                return new PassContext(InMemoryDbContextOptionsFactory.Create<PassContext>());
+            }
             [Fact]
             public void Pass_is_saved_to_the_database_for_each_pass()
             {
-                using (PassContext context =
-                        new PassContext(InMemoryDbContextOptionsFactory.Create<PassContext>()))
+                using (PassContext context = GetContext())
                 {
                     var mediator = new Mock<IMediator>();
                     var dateService = new Mock<IDateService>();
                     var currentDate = DateTime.UtcNow;
                     dateService.Setup(x => x.Now()).Returns(currentDate);
                     var sut = new AddSkiPassOnPurchaseCompleted(context, mediator.Object, dateService.Object);
-                    var passPurchased = new PassPurchased
-                    {
-                        CardId = 1,
-                        PassTypeId = 2,
-                        DiscountCode = "2016springpromotion",
-                        PricePaid = 200m
-                    };
                     sut.Handle(new Events.PurchaseCompleted
                     {
                         Passes = new List<PassPurchased>
@@ -54,39 +58,60 @@ namespace AlpineSkiHouse.Web.Tests.Handlers
             public void PassesAddedEvents_is_published_for_each_pass()
             {
                 using (PassContext context =
-                        new PassContext(InMemoryDbContextOptionsFactory.Create<PassContext>()))
+                        GetContext())
                 {
                     var mediator = new Mock<IMediator>();
                     var dateService = new Mock<IDateService>();
                     var currentDate = DateTime.UtcNow;
                     dateService.Setup(x => x.Now()).Returns(currentDate);
                     var sut = new AddSkiPassOnPurchaseCompleted(context, mediator.Object, dateService.Object);
-                    var passPurchased = new PassPurchased
-                    {
-                        CardId = 1,
-                        PassTypeId = 2,
-                        DiscountCode = "2016springpromotion",
-                        PricePaid = 200m
-                    };
+
                     sut.Handle(new Events.PurchaseCompleted
                     {
                         Passes = new List<PassPurchased>
-                        {
-                            passPurchased
-                        }
+            {
+                passPurchased
+            }
                     });
                     var dbPass = context.Passes.Single();
                     mediator.Verify(x => x.Publish(It.Is<PassAdded>(y => y.CardId == passPurchased.CardId &&
-                                                                         y.CreatedOn == currentDate &&
-                                                                         y.PassId == dbPass.Id &&
-                                                                         y.PassTypeId == passPurchased.PassTypeId)));
+                                                                            y.CreatedOn == currentDate &&
+                                                                            y.PassId == dbPass.Id &&
+                                                                            y.PassTypeId == passPurchased.PassTypeId)));
                 }
             }
 
             [Fact]
             public void Empty_passes_collection_saves_nothing_to_the_database()
             {
-                throw new NotImplementedException();
+                using (PassContext context = GetContext())
+                {
+                    var mediator = new Mock<IMediator>();
+                    var dateService = new Mock<IDateService>();
+                    var currentDate = DateTime.UtcNow;
+                    dateService.Setup(x => x.Now()).Returns(currentDate);
+                    var sut = new AddSkiPassOnPurchaseCompleted(context, mediator.Object, dateService.Object);
+                    sut.Handle(new Events.PurchaseCompleted { Passes = new List<PassPurchased>() });
+
+                    Assert.Equal(0, context.Passes.Count());
+                }
+            }
+
+            [Fact]
+            public void Empty_passes_collection_publishes_no_messages()
+            {
+                using (PassContext context = GetContext())
+                {
+                    var mediator = new Mock<IMediator>();
+                    var dateService = new Mock<IDateService>();
+                    var currentDate = DateTime.UtcNow;
+                    dateService.Setup(x => x.Now()).Returns(currentDate);
+                    var sut = new AddSkiPassOnPurchaseCompleted(context, mediator.Object, dateService.Object);
+
+                    sut.Handle(new Events.PurchaseCompleted { Passes = new List<PassPurchased>() });
+
+                    mediator.Verify(x => x.Publish(It.IsAny<PassAdded>()), Times.Never);
+                }
             }
         }
     }
