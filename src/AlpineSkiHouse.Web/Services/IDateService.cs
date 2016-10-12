d2110de1e5063d7f6067b851069269660f1ff2eb
Simon Timms <stimms@gmail.com>
2016-10-11 22:57:24 -0600

    Adding a couple of characteriaztion tests

---
+++ src/AlpineSkiHouse.Web/Services/IDateService.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AlpineSkiHouse.Services
+{
+    public interface IDateService
+    {
+        DateTime Now();
+    }
+}
