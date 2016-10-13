72a71ff785e7c2857afe27e7c9adbf2ed7a1c829
David Paquette <david.paquette@usask.ca>
2016-10-12 20:54:28 -0600

    A simple example of middleware (#73)
    * An example of middleware
    
    * Remove unused using statements

---
+++ src/AlpineSkiHouse.Web/Middleware/CSPMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace AlpineSkiHouse.Middleware
+{
+    public class CSPMiddleware
+    {
+        private readonly RequestDelegate _next;
+                
+        public CSPMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'");
+            await _next.Invoke(context);
+        }
+
+    }
+}
