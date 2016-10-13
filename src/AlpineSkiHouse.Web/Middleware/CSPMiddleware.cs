515daa65b8676d7a4929944c1a11709de5784a72
David Paquette <dave@davepaquette.com>
2016-10-12 20:45:48 -0600

    An example of middleware

---
+++ src/AlpineSkiHouse.Web/Middleware/CSPMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlpineSkiHouse.Middleware
+{
+    public class CSPMiddleware
+    {
+        private readonly RequestDelegate _next;
+                
+        public CSPMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'");
+            await _next.Invoke(context);
+        }
+
+    }
+}
