e4f20b749b68b53fc3d6e368fe0ad4d0cf592337
Simon Timms <stimms@gmail.com>
2016-11-15 21:06:30 -0700

    Fixes for javascript loading

--- src/AlpineSkiHouse.Web/Scripts/jspmconfig.js
+++ src/AlpineSkiHouse.Web/Scripts/jspmconfig.js
@@ -9,8 +9,8 @@ System.config({
     ]
   },
   paths: {
-    "npm:*": "npm/*",
-    "github:*": "github/*"
+    "npm:*": "../jspm_packages/npm/*",
+    "github:*": "../jspm_packages/github/*"
   },
 
   map: {
@@ -18,7 +18,13 @@ System.config({
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "chart": "github:chartjs/Chart.js@2.2.2",
     "core-js": "npm:core-js@1.2.7",
-    "jquery": "npm:jquery@3.1.0",
+    "jquery": "npm:jquery@3.1.1",
+    "jquery-validation": "github:jzaefferer/jquery-validation@1.15.1",
+    "jquery-validation-unobtrusive": "github:aspnet/jquery-validation-unobtrusive@3.2.6",
+    "twbs/bootstrap": "github:twbs/bootstrap@4.0.0-alpha",
+    "github:aspnet/jquery-validation-unobtrusive@3.2.6": {
+      "jquery-validation": "github:jzaefferer/jquery-validation@1.15.1"
+    },
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
     },
@@ -36,6 +42,12 @@ System.config({
     },
     "github:jspm/nodelibs-vm@0.1.0": {
       "vm-browserify": "npm:vm-browserify@0.0.4"
+    },
+    "github:jzaefferer/jquery-validation@1.15.1": {
+      "jquery": "npm:jquery@3.1.1"
+    },
+    "github:twbs/bootstrap@4.0.0-alpha": {
+      "jquery": "github:components/jquery@3.1.1"
     },
     "npm:assert@1.4.1": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
