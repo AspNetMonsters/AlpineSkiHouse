20f4a003e10a680c73beed408e28405ea00d7d9b
David Paquette <dave@davepaquette.com>
2016-12-03 10:37:45 -0700

    Event and handler for logging card scans to the database

---
+++ src/AlpineSkiHouse.Web/Controllers/PassValidationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AlpineSkiHouse.Web.Services;
+
+namespace AlpineSkiHouse.Web.Controllers
+{
+    public class PassValidationController : Controller
+    {
+        private readonly IPassValidityChecker _passValidityChecker;
+
+        public PassValidationController(IPassValidityChecker passValidityChecker)
+        {
+            _passValidityChecker = passValidityChecker;
+        }        
+    }
+}
---
+++ src/AlpineSkiHouse.Web/Events/CardScanned.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+
+namespace AlpineSkiHouse.Events
+{
+    /// <summary>
+    /// An event notification that occurs when a Ski Card has been scanned at a particular location
+    /// </summary>
+    public class CardScanned : INotification
+    {
+        public int CardId { get; set; }
+        public int LocationId { get; set; }
+    }
+}
